201362c3539c4a7673fab13be3d3825eed4157a3
maryhain <41811344+maryhain@users.noreply.github.com>
2022-09-06 00:11:50 -0500

    test

---
+++ Test/PassageAnalyzerTest2.java
@@ -0,0 +1,22 @@
+package MobyDickNovel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PassageAnalyzerTest2 {
+	
+
+	    void cleanWord() {
+	    }
+
+	    
+	    void getReport() {
+	    }
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Test/Test1.java
@@ -0,0 +1,23 @@
+package MobyDickNovel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+	
+
+	    void createReport() {
+	    }
+
+	    
+	    void analyze() {
+	    }
+	
+	@Test
+	void test1() {
+		fail("didnot work!!");
+	}
+
+}
+
